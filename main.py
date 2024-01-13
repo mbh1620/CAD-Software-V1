aff3ff6bd19b22f9871f4fe7afa46985123804aa
Matthew Haywood <matthaywood2012@gmail.com>
2024-01-13 12:54:20 +0000

    initial commit and repostitory setup

---
+++ main.py
@@ -0,0 +1,16 @@
+from Classes.ProjectionViewer import ProjectionViewer
+from Classes.Wireframe import Wireframe
+import numpy as np
+
+centerPoint = Wireframe()
+
+centerPoint.addNodes([[0,0,0]])
+
+projectionViewer1 = ProjectionViewer(1200,1000, centerPoint)
+	
+projectionViewer1.addWireframe('centerPoint', centerPoint)
+
+projectionViewer1.openFile('./blenderCube.obj', 100)
+projectionViewer1.wireframes['./blenderCube.obj2'].displayNodes = False
+
+projectionViewer1.run()
